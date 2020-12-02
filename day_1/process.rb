2392632c3cb7fbec1fe1b445a83ac00a92211462
Ricky Dalziel <ricky@floatapp.com>
2020-12-02 12:31:16 +0000

    Tidy up day 1 code

--- day_1/process.rb
+++ day_1/process.rb
@@ -4,24 +4,21 @@ def read_values
   File.read('input').split("\n").map(&:to_i)
 end
 
-def two_values_product
-  values = read_values
-
+def two_values_product(values)
   values_size = values.count
 
   # looks minging but counts each unique permutation once
   first_values = values[0, values_size - 3]
   first_values.each_with_index do |v1, index1|
     second_values = values[index1, values_size - 2]
-    second_values.each_with_index do |v2, index2|
+    second_values.each do |v2|
       set = [v1, v2]
       puts set.inject(:*) if set.sum == 2020
     end
   end
 end
 
-def three_values_product
-  values = read_values
+def three_values_product(values)
   values_size = values.count
 
   # looks minging but counts each unique permutation once
@@ -30,7 +27,7 @@ def three_values_product
     second_values = values[index1, values_size - 2]
     second_values.each_with_index do |v2, index2|
       third_values = second_values[index2, values_size - 1]
-      third_values.each_with_index do |v3, index3|
+      third_values.each do |v3|
         set = [v1, v2, v3]
         puts set.inject(:*) if set.sum == 2020
       end
@@ -38,5 +35,6 @@ def three_values_product
   end
 end
 
-two_values_product
-three_values_product
+values = read_values
+two_values_product(values)
+three_values_product(values)
